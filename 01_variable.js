578251d880b7d497a75fae2befafad7749cb61c8
ziasial007 <reziaurrehman@gmail.com>
2024-12-17 23:43:57 +0500

    hiesh lecture

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+const id = 123456
+let village = "basti sial"
+var city = 'aminabad'
+accout = 2
+
+village ="zwww"
+city = "swwwwww"
+console.table([village, city, id]);
+
+console.log(id);
+
+// NOTE 
+//1 {} this currlry bracket is //Scope// 
+//2 not use Var use onliy const and let
---
+++ 02 datatypes.js
@@ -0,0 +1,27 @@
+//"USE STRICT" es ka matlab ye new tareeqa hay yani ye new virsion hay 
+
+
+  //let name = "zia sial" // string data type 
+  // let number = 124433  //  number
+  // let isLoginIn = true/false // bolean type
+
+
+//how may tpye of data types//
+
+// DATA TYPES //
+// number  => 2 power 52
+//BigInt // this is for big value of number (like trading stokmarkeet)
+//String // "" ''
+//Boolean // true/false
+//null // this is  standalone value / data typs
+//undefind // like====> let zia;
+//Symbol // unique ... jab bhe ham unique kay baray mai bat kray gay samjo ham symbol kay baray mai bat kray gay
+
+//Object // imporatan topic
+
+
+//console.log(typeof ''); // string
+//console.log(typeof null); // object
+
+
+
